e3fa2f923fab20c2f35973248512ba31d67419ec
bewba <lumaguihans0123@gmail.com>
2024-03-17 15:35:10 +0800

    Imtegrated the stock market API

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ currTicker = "MSFT"
 
 api_key = admin.getTicker()
 
+def getStockPrice(ticker_symbol, api):
+    url = f"https://api.twelvedata.com/time_series?symbol={ticker_symbol}&interval=1min&apikey={api}"
+    response = requests.get(url).json()
+    return response
+
+print(getStockPrice(currTicker,api_key))
