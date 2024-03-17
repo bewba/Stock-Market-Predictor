616ed76289822ed1a6602f8d95a1b995abf70c1c
bewba <lumaguihans0123@gmail.com>
2024-03-17 16:49:44 +0800

    Jupyter Notebook integrated + stock list collection

--- main.py
+++ main.py
@@ -2,25 +2,33 @@ import requests
 import time
 import admin
 import json
-
-
+import pandas as pd
+import csv
 
 currTicker = "MSFT"
 
 api_key = admin.getTicker()
 
+# Finding Stock Price at a timeframe
+# Incomplete
 def getStockPrice(ticker_symbol, api):
     url = f"https://api.twelvedata.com/time_series?symbol={ticker_symbol}&interval=1min&apikey={api}"
     response = requests.get(url).json()
     return response
 
+
+# Should send a CSV for the list of stocks
+# Incomplete
 def getStockList():
     url = f"https://api.twelvedata.com/stocks"
     response = requests.get(url).json()
+    data = response['data']
     try:
-        with open("stocklist.txt", "w") as file:
-            json.dump(response, file)
-            print("data stored successfully")
+        with open('stocklist.csv', 'w', newline="") as f:
+            writer = csv.DictWriter(f,fieldnames=data.keys())
+            writer.writeheader()
+            writer.writerow(data)
+        print("success")
     except:
         print("An Error Occured")
     return response
